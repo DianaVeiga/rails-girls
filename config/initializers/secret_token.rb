f0a88cce5642f9139370195da74ec174af497c4f
Diana Veiga <diana.d.veiga@gmail.com>
2014-10-12 16:14:47 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railsgrils::Application.config.secret_key_base = '748f51d00752c86b9641e9308f62c166b22b6acda5306ff86faef1563d6590200239de0ad2d73a9617933cda52c3455dfb21e00655e6098c0fb3d141a1613d5f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Railsgrils::Application.config.session_store :cookie_store, key: '_railsgrils_session'
